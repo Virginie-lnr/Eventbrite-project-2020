0bcfe88ba6f06fbf7f0fa2fb88ca6c1e779acb12
Virginie <virginie.lenoir17@gmail.com>
2020-05-12 16:42:32 +0200

    changed sign out button

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'events#index'
+  
+  devise_for :users
   resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
