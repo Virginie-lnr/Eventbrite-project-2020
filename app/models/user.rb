e7940c1e0fe3dac1c3483b046d673c51b999225d
Virginie <virginie.lenoir17@gmail.com>
2020-05-12 19:19:25 +0200

    added email in devise.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,16 @@ class User < ApplicationRecord
 
   after_create :welcome_send
 
-  def welcome_send
-    UserMailer.welcome_email(self).deliver_now
-  end
-
   has_many :attendances
   has_many :events, through: :attendances
 
   has_many :administrated_events, foreign_key: "admin_id", class_name: "Event"
+
+    def welcome_send
+      UserMailer.welcome_email(self).deliver_now
+    end
+
+    # def welcome_reset_password_instructions
+    #   UserMailer.welcome_reset_password_instructions(User.first)
+    # end
 end
