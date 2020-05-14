3cfcb7490e34bc778fcf91bb46c6537a5a499ada
Virginie <virginie.lenoir17@gmail.com>
2020-05-14 19:50:26 +0200

    added avatar for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
 
   has_many :events, foreign_key: 'admin_id', class_name: 'Event'
 
+  has_one_attached :avatar
+
     def welcome_send
       UserMailer.welcome_email(self).deliver_now
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'avatars/create'
   # get 'users/show'
   root to: 'events#index'
 
@@ -7,8 +8,9 @@ Rails.application.routes.draw do
   resources :events
   post 'events/subscribe/:id', to: 'events#subscribe', as: 'subscribe'
 
-
-  resources :users
+  resources :users do
+    resources :avatars, only: [:create]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
